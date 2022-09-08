45de4226e3dad337ea3cd11a6bf7b6585b64787d
SaiMark <59070894+SaiMark@users.noreply.github.com>
2022-09-08 12:27:54 +0930

    update for step-01
    Programming for C

---
+++ STEP-01/variable.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+	char characterName [] = "John";
+	int characterAge = 35;
+	printf("There once was a man named %s\n", characterName);
+	printf("he was %d years old.\n",characterAge);
+
+	characterAge = 30;
+	printf("He really liked the name John\n");
+	printf("but did not like being %d.\n",characterAge);
+
+	int age = 40;
+	double gpa = 3.6;
+	char grade = 'A';
+
+
+	return 0;
+}
